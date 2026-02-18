3fec4e83442c8c66de83952f1ea6e2e5799f0d15
unknown <mickogamer2006@gmail.com>
2026-02-18 12:58:40 +0800

    Update myproject.py with new information

--- myproject.py
+++ myproject.py
@@ -1,1 +1,8 @@
+
 print("Hello, World!")
+# This is a simple Python script that prints "Hello, World!" to the console. You can run this script in any Python environment to see the output.
+# You can also add more functionality to this script, such as taking user input or performing calculations. For example:
+# Taking user input
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+# Performing calculations
